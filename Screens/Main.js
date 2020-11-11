5b89e7b0357ff46ed239f301a4bb3501134548f1
heidiWesty <heidi.diggs@selu.edu>
2020-11-10 23:56:59 -0600

    main page pic

--- Screens/Main.js
+++ Screens/Main.js
@@ -5,6 +5,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  ImageBackground,
 } from "react-native";
 
 import * as ScreenOrientation from 'expo-screen-orientation';
@@ -16,7 +17,26 @@ export default class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <View
+          style={{
+            flex: 1,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <ImageBackground
+            source={require("../Assets/MainPic.jpg")}
+            //resizeMode="contain"
+            style={{ flex: 1, width: null, height: null }}
+          ></ImageBackground>
+        </View>
+        <View style={{alignItems: 'center', marginBottom: 30}}>
         <Text style={styles.text}>This is the Main Page</Text>
+        </View>
+        <View style={{alignItems: 'center', padding: 20}}>
         <TouchableOpacity
           style={styles.button}
           onPress={() => this.props.navigation.navigate("Page_1")}
@@ -28,12 +48,14 @@ export default class Main extends React.Component {
                 textAlign: "center",
                 textAlignVertical: "center",
                 fontSize: 18,
+                fontFamily: 'serif'
               },
             ]}
           >
             Begin here
           </Text>
         </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -42,22 +64,23 @@ export default class Main extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "flex-end",
   },
 
   button: {
     height: 40,
     width: 130,
     padding: 5,
-    paddingTop: 5,
     paddingBottom: 5,
-    marginTop: 20,
-    backgroundColor: 'blue',
-    justifyContent: "center",
+    backgroundColor: '#983628',
+    borderRadius: 20,
+    marginBottom: 70
+    
   },
   text: {
-    fontSize: 18,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'serif'
   },
 });
